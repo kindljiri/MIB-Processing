936bea09dd8045e0294f8d3bb4ef60554bc27698
kindljiri <kindl_jiri@yahoo.com>
2020-04-27 12:06:10 +0200

    Correcting Parsing Bugs
    Implemented new functions
    1) to  clean MIB and get tokens 
    2) now parser works just with tokens (instead of whole lines.

--- MIB-Processing.psm1
+++ MIB-Processing.psm1
@@ -1,20 +1,502 @@
 ﻿
 #Recursion function to expand the OID from (and translate into numbers)
 function Get-OID($object, $mib) {
+  #DEBUG
+  #Write-Host $object.objectName
   $parent_name = $object.parent
   $parent_object = $mib | where objectName -EQ $parent_name
   if ($parent_object) {
     $parentOID = Get-OID $parent_object $mib
     $OID = $parentOID + '.' + $object.ID
+    #Write-Host  $OID
     return $OID
   }
   else {
+    #Write-Host $object.OID
     return $object.OID
   }
 }
 
+function Sanitize-MIB-Text($lines) {
+  $status = 'init'
+  $cleanMIBtext = @()
+  $tokens = @()
+  #Clean the lines, remove comments, parse strings and arrays
+  foreach ($line in $lines) {
+    #remove leading and tailing spaces 
+    $line = $line.trim() 
+    #remove duplicate white spaces (replace them with one space)
+    $line = $line -replace '\s+', " "
+    #unify comments because some stupids are able to use this --- Comment (as -- are opening the comment and next -- on the line are end it's valid comment)
+    #ASN.1 comments commence with a pair of adjacent hyphens and end with the next pair of adjacent hyphens or at the end of the line, whichever occurs first.
+    #As replace seems to be recursive removing 4 hyphens will remove all 
+    $line = $line -replace '-{4}', ''
+    #And then replacing 3 with 2
+    $line = $line -replace '-{3}', '--'
+    #And then finally
+    #remove comments
+    if ($line.startswith("--")) {
+      if ($line.indexOf("--") -eq $line.lastIndexOf("--")){
+        continue
+      }
+      else {
+        $cleanMIBtext += $line.subString($line.lastIndexOf("--")+2,$line.Length-$line.lastIndexOf("--")-2)
+      }
+    }
+    if ($line -match '--') {
+      if ($line.indexOf("--") -eq $line.lastIndexOf("--")){
+        $cleanMIBtext += $line.subString(0,$line.lastIndexOf("--"))
+        continue
+      }
+      else {
+        $cleanMIBtext += $line.subString(0,$line.lastIndexOf("--"))
+        $cleanMIBtext += $line.subString($line.lastIndexOf("--")+2,$line.Length-$line.lastIndexOf("--")-2)
+        continue
+      }
+    }
+    #lets find and process strings, "arrays" (assignment is special array
+    if ($status -eq 'init'){
+      #processing string
+      if ($line -match '"') {
+        if ($line.indexOf('"') -gt 0) {
+          $cleanMIBtext += $line.substring(0,($line.indexOf('"')))
+          $text = $line.substring($line.indexOf('"'),$line.length-$line.indexOf('"'))
+          if ($text.indexOf('"') -eq $text.lastIndexOf('"')) {
+            $status = 'text_processing'
+          }
+          else {
+            $cleanMIBtext += $text.substring(0,($text.lastIndexOf('"')+1))
+            if ($text.lastIndexOf('"')+1 -lt $text.Length) {
+              $cleanMIBtext += $text.substring($text.lastIndexOf('"'),$text.Length-$text.lastIndexOf('"'))
+            }
+            
+          }
+        }
+        else {
+          if ($line.indexOf('"') -eq $line.lastIndexOf('"')) {
+            $text = $line
+            $status = 'text_processing'
+          }
+          else {
+            $cleanMIBtext += $line.substring(0,($line.lastIndexOf('"')+1))
+            if ($line.lastIndexOf('"')+1 -lt $line.Length) {
+              $cleanMIBtext += $line.substring($line.lastIndexOf('"'),$line.Length-$line.lastIndexOf('"'))
+            }
+          }
+          
+        }
+        
+      }
+      #procesing "array"
+      elseif ($line -match '{') {
+        if ($line.indexOf('{') -gt 0) {
+          $cleanMIBtext += $line.substring(0,($line.indexOf('{')))
+          $array = $line.substring($line.indexOf('{'),$line.Length-$line.indexof('{'))
+          if ($array -match '}') {
+            $cleanMIBtext += $array.substring(0,$array.indexOf('}')+1)
+            if ($array.indexOf('}')+1 -lt $array.Length) {
+              $cleanMIBtext += $array.substring($array.indexOf('}'),$array.Length-$array.indexof('}'))
+            }
+          }
+          else {
+            $status = 'array_processing'
+          }
+        }
+        else {
+          if ($line -match '}') {
+            $cleanMIBtext += $line.substring(0,$line.indexOf('}')+1)
+            if ($line.indexOf('}')+1 -lt $line.Length) {
+              $cleanMIBtext += $line.substring($line.indexOf('}'),$line.Length-$line.indexof('}'))
+            }
+          $array = $line
+          $status = 'array_processing'
+        }
+      }
+      }
+      else {
+        $cleanMIBtext += $line
+      }
+    }
+    #processing multiline strings
+    elseif ($status -eq 'text_processing') {
+      if ($line -match '"') {
+        if ($line.lastIndexOf('"')+1 -eq $line.length) {
+          $text += $line
+          $cleanMIBtext += $text
+        }
+        else {
+          $text += " " + $line.substring(0,$line.lastIndexOf('"')+1)
+          $cleanMIBtext += $text
+          $cleanMIBtext += $line.substring($line.lastIndexOf('"'),$line.length-$line.lastIndexOf('"'))
+        }
+        $status = 'init'
+      }
+      else {
+        $text += " $line"
+      }
+    }
+    #processing multiline array
+    elseif ($status -eq 'array_processing') {
+      if ($line -match '}') {
+        $array += " $line"
+        $status = 'init'
+        $cleanMIBtext += $array
+      }
+      else {
+        $array +=" $line"
+      }
+    }
+    
+  }
+
+  $lines = @()
+  $lines = $cleanMIBtext
+  $cleanMIBtext = @()
+
+  #One more round of cleaning, left extra spaces and empty lines
+  foreach ($line in $lines) {
+    $line = $line.trim() 
+    #remove duplicate white spaces (replace them with one space)
+    $line = $line -replace '\s+', " "
+    if ($line -eq '') {
+      continue
+    }
+    $cleanMIBtext += $line
+  }
+
+  #tokenize: One token per line (string is one line, array is one line)
+  foreach ($line in $cleanMIBtext) {
+    if ($line.startswith('"')) {
+      $tokens += $line
+    }
+    elseif ($line.startswith('{')) {
+      $tokens += $line
+    }
+    else {
+      $tokens += $line.split()
+    }
+  }
+
+  $cleanMIBtext = @()
+  $cleanMIBtext = $tokens 
+  $tokens = @()
+  
+  #remove empty lines and extra spaces
+  foreach ($line in $cleanMIBtext) {
+    $line = $line.trim()
+    if ($line -eq '') {
+      continue
+    }
+    $tokens += $line
+  }
+  return $tokens
+}
+
+function New-Parse-MIB($tokens) {
+  #based on:
+  # rfc2578 - SMIv2
+  # rfc1215 - TRAP-TYPE
+  $sa_status="init"
+  ($object_name,$object_type,$object_syntax,$status,$description,$objects,$ID,$parent,$object_max_access,$object_units,$object_reference,$object_index,$object_augments,$object_defval,$notification_objects,$parent,$ID) = ("","","","","","","","","","","","","","","","","")
+  $counter = 0
+  $mib = @()
+
+  #below is not used yet (will see if I'll use it later
+  $notification_type_tokens = ('OBJECTS','STATUS','DESCRIPTION','REFERENCE','::=')
+  $object_type_tokens=('SYNTAX','UNITS','MAX-ACCESS','STATUS','DESCRIPTION','REFERENCE','INDEX','AUGMENTS','DEFVAL','::=')
+  $trap_type_tokens = ('ENTERPRISE','VARIABLES','DESCRIPTION','REFERENCE')
+  
+  #below is union of above arrays
+  $expected_type_tokens=('ENTERPRISE','VARIABLES','OBJECTS','SYNTAX','UNITS','MAX-ACCESS','STATUS','DESCRIPTION','REFERENCE','INDEX','AUGMENTS','DEFVAL','::=')
+
+  foreach ($token in $tokens) {
+    #just for sure
+    $token = $token.trim()
+    if ($sa_status -eq "init") {
+      if ($token -eq 'BEGIN') {
+        if ($tokens[$counter-2]+$tokens[$counter-1]+$token -eq 'DEFINITIONS::=BEGIN') {
+          $module_name = $tokens[$counter-3]
+        }
+      }
+      elseif ($token -eq 'IMPORT') {
+        $sa_status = 'IMPORT'
+      }
+      elseif ($token -eq 'MODULE-IDENTITY') {
+        $sa_status = 'MODULE-IDENTITY'
+        $object_type = "MODULE-IDENTITY"
+        $object_name = $tokens[$counter-1]
+      }
+      elseif ($token -eq 'IDENTIFIER' -and $tokens[$counter-1] -eq 'OBJECT') {
+        $sa_status = 'OBJECT IDENTIFIER'
+        $object_type = "OBJECT IDENTIFIER"
+        $object_name = $tokens[$counter-2]
+      }
+      elseif ($token -eq 'OBJECT-IDENTITY') {
+        $sa_status = 'OBJECT IDENTIFIER'
+        $object_type = "OBJECT IDENTIFIER"
+        $object_name = $tokens[$counter-1]
+      }
+      elseif ($token -eq 'OBJECT-TYPE') {
+        $sa_status = 'OBJECT-TYPE'
+        $object_type = "OBJECT-TYPE"
+        $object_name = $tokens[$counter-1]
+      }
+      elseif ($token -eq 'NOTIFICATION-TYPE') {
+        $sa_status = 'NOTIFICATION-TYPE'
+        $object_type = "NOTIFICATION-TYPE"
+        $object_name = $tokens[$counter-1]
+      }
+      elseif ($token -eq 'TRAP-TYPE') {
+        $sa_status = 'TRAP-TYPE'
+        $object_type = "TRAP-TYPE"
+        $object_name = $tokens[$counter-1]
+      }
+      $object_name = $object_name.trim()
+      $counter += 1
+      continue 
+    }
+    elseif ($sa_status -eq 'IMPORT') {
+      if ($token.endswith(';')) {
+        $sa_status = 'init'
+      }
+    } 
+    elseif ($sa_status -eq 'MODULE-IDENTITY') {
+      if ($token -eq '::=') {
+        $sa_status='::='
+        $counter += 1
+        continue
+      }
+    }
+    elseif ($sa_status -eq 'OBJECT IDENTIFIER') {
+      if ($token -eq '::=') {
+        $sa_status='::='
+        $counter += 1
+        continue
+      }
+      
+    }
+    elseif ($sa_status -eq 'OBJECT-TYPE') {
+      foreach ($ott in $expected_type_tokens) {
+        if ($ott -eq $token) {
+          $sa_status = $token 
+        }
+      }
+      $counter += 1
+      continue
+    }
+    elseif ($sa_status -eq 'NOTIFICATION-TYPE') {
+      foreach ($ott in $expected_type_tokens) {
+        if ($ott -eq $token) {
+          $sa_status = $token 
+        }
+      }
+      $counter += 1
+      continue
+    }
+    elseif ($sa_status -eq 'SYNTAX') {
+      foreach ($ott in $expected_type_tokens) {
+        if ($ott -eq $token) {
+          $sa_status = $token
+          $object_syntax = $object_syntax.trim() 
+        }
+      }
+      if ($sa_status -eq 'SYNTAX') {
+        $object_syntax += $token + " "
+      }
+      else {
+        #unexpected token
+      }
+      $counter += 1
+      continue
+    }
+    elseif ($sa_status -eq 'UNITS') {
+      foreach ($ott in $expected_type_tokens) {
+        if ($ott -eq $token) {
+          $sa_status = $token
+          $object_units = $object_units.trim() 
+        }
+      }
+      if ($sa_status -eq 'UNITS') {
+        $object_units += $token + " "
+      }
+      else {
+        #unexpected token
+      }
+      $counter += 1
+      continue
+    }
+    elseif ($sa_status -eq 'MAX-ACCESS') {
+      foreach ($ott in $expected_type_tokens) {
+        if ($ott -eq $token) {
+          $sa_status = $token
+          $object_max_access = $object_max_access.trim() 
+        }
+      }
+      if ($sa_status -eq 'MAX-ACCESS') {
+        $object_max_access += $token + " "
+      }
+      else {
+        #unexpected token
+      }
+      $counter += 1
+      continue
+    }
+    elseif ($sa_status -eq 'STATUS') {
+      foreach ($ott in $expected_type_tokens) {
+        if ($ott -eq $token) {
+          $sa_status = $token
+          $status = $status.trim() 
+        }
+      }
+      if ($sa_status -eq 'STATUS') {
+        $status += $token + " "
+      }
+      else {
+        #unexpected token
+      }
+      $counter += 1
+      continue
+    }
+    elseif ($sa_status -eq 'DESCRIPTION') {
+      foreach ($ott in $expected_type_tokens) {
+        if ($ott -eq $token) {
+          $sa_status = $token 
+        }
+      }
+      if ($sa_status -eq 'DESCRIPTION' -and $token.startswith('"')) {
+        $description = $token
+      }
+      else {
+        #unexpected token
+      }
+      $counter += 1
+      continue
+    }
+    elseif ($sa_status -eq 'REFERENCE') {
+      foreach ($ott in $expected_type_tokens) {
+        if ($ott -eq $token) {
+          $sa_status = $token
+          $object_reference = $object_reference.trim() 
+        }
+      }
+      if ($sa_status -eq 'REFERENCE') {
+        $object_reference += $token + " "
+      }
+      else {
+        #unexpected token
+      }
+      $counter += 1
+      continue
+    }
+    elseif ($sa_status -eq 'INDEX') {
+      foreach ($ott in $expected_type_tokens) {
+        if ($ott -eq $token) {
+          $sa_status = $token
+          $object_index = $object_index.trim() 
+        }
+      }
+      if ($sa_status -eq 'INDEX') {
+        $object_index += $token + " "
+      }
+      else {
+        #unexpected token
+      }
+      $counter += 1
+      continue
+    }
+    elseif ($sa_status -eq 'AUGMENTS') {
+      foreach ($ott in $expected_type_tokens) {
+        if ($ott -eq $token) {
+          $sa_status = $token
+          $object_augments = $object_augments.trim() 
+        }
+      }
+      if ($sa_status -eq 'AUGMENTS') {
+        $object_augments += $token + " "
+      }
+      else {
+        #unexpected token
+      }
+      $counter += 1
+      continue
+    }
+    elseif ($sa_status -eq 'DEFVAL') {
+      foreach ($ott in $expected_type_tokens) {
+        if ($ott -eq $token) {
+          $sa_status = $token
+          $object_defval = $object_defval.trim() 
+        }
+      }
+      if ($sa_status -eq 'DEFVAL') {
+        $object_defval += $token + " "
+      }
+      else {
+        #unexpected token
+      }
+      $counter += 1
+      continue
+    }
+    elseif ($sa_status -eq 'OBJECTS' -or $sa_status -eq 'VARIABLES') {
+      foreach ($ott in $expected_type_tokens) {
+        if ($ott -eq $token) {
+          $sa_status = $token
+          $object_defval = $object_defval.trim() 
+        }
+      }
+      if ($sa_status -eq 'OBJECTS' -and $token.startswith('{')) {
+        $notification_objects = $token
+      }
+      else {
+        #unexpected token
+      }
+      $counter += 1
+      continue
+    }
+    elseif ($sa_status -eq 'ENTERPRISE') {
+      foreach ($ott in $expected_type_tokens) {
+        if ($ott -eq $token) {
+          $sa_status = $token
+          $parent = $parent.trim() 
+        }
+      }
+      if ($sa_status -eq 'ENTERPRISE') {
+        $parent = $token
+      }
+      else {
+        #unexpected token
+      }
+      $counter += 1
+      continue
+    }
+    elseif ($sa_status -eq '::=') {
+      if ($token.startswith('{')) {
+        $token = $token -replace '{', ''
+        $token = $token -replace '}', ''
+        $token = $token.trim()
+        if ($object_type -eq 'TRAP-TYPE') {
+          $ID = $ID.trim()
+        }
+        else {
+          ($parent,$ID) = ($token -split " ")
+        }
+        $parent = $parent.trim()
+        $ID = $ID.trim()
+        $objectProperties = @{ objectName = $object_name; objectType = $object_type; objectSyntax = $object_syntax; status = $status; description = $description; objects = $notification_objects; ID = $ID; parent = $parent; OID = "$parent.$ID"; module = $module_name; objectFullName = "$module_name::$object_name" }
+        $object = New-Object psobject -Property $objectProperties
+        $mib += $object
+        $sa_status="init"
+        ($object_name,$object_type,$object_syntax,$status,$description,$objects,$ID,$parent,$object_max_access,$object_units,$object_reference,$object_index,$object_augments,$object_defval,$notification_objects,$parent,$ID) = ("","","","","","","","","","","","","","","","","")
+        $counter += 1
+        continue
+      }
+    }
+    $counter += 1
+  }
+  return $mib
+}
+
 #Takes the lines from MIB file, and parse them into objects, returns the array of custom objects
-function Parse-MIB($lines) {
+function Old-Parse-MIB($lines) {
   $sa_status="init"
   $object_name = ""
   $object_type = ""
@@ -31,8 +513,8 @@ function Parse-MIB($lines) {
     $line = $line.trim() 
     $line = $line -replace '\s+', " "
     #"DEBUG: $line"
-    #ignore line containings only comment
     #"DEBUG: $sa_status"
+    #ignore line containings only comment
     if ($line.startswith("--")) {
       continue
     }
@@ -403,7 +885,7 @@ function Import-MIB {
     Module Name    : MIB-Processing  
     Author         : Jiri Kindl; kindl_jiri@yahoo.com
     Prerequisite   : PowerShell V2 over Vista and upper.
-    Version        : 20200305
+    Version        : 20200426
     Copyright 2020 - Jiri Kindl
 .LINK  
     
@@ -471,15 +953,23 @@ function Import-MIB {
     }
   }
   END {
-    $mib = Parse-MIB $lines
+    #$mib = Old-Parse-MIB $lines
+    $tokens = Sanitize-MIB-Text $lines
+    $mib = New-Parse-MIB $tokens
 
+    #expand OID from parent.ID to full path in MIB scope
     foreach ($object in $mib) {
       $OID = Get-OID $object $mib
+      #DEBUG
+      #$object.objectName
+      #$OID
+      #DEBUG
       $objectProperties = @{ objectName = $object.ObjectName; OID = $OID }
       $new_object = New-Object psobject -Property $objectProperties
       $OIDs += $new_object
     }
-  
+
+    #resolve OIDs to numbers (using OID repo file) And Update OID repo if switch UpdateOIDRepo is used
     if ($OIDRepo) {
       foreach ($oid_record in $OIDs) {
         $oid=$oid_record.OID
@@ -504,6 +994,7 @@ function Import-MIB {
       }
     }
 
+    #update find resolved OIDs to corresponding objects
     foreach ($object in $OIDs) {
       $mib_object = $mib | where objectName -EQ $object.objectName
       $mib_object.OID = $object.OID
